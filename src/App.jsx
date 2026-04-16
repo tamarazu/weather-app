f49ffaa2ab03d6407b20fd66bcbb254ff023f5e8
tamarazu <tamarazulaika.u@gmail.com>
2026-04-16 22:38:21 +0700

    done UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import { useState } from "react";
-import { Search } from "lucide-react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
-import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  // return Home();
   return (
     <div className="min-h-screen bg-[url('https://images.unsplash.com/photo-1504608524841-42fe6f032b4b?auto=format&fit=crop&w=1920&q=80')] bg-cover bg-center p-4 font-sans flex content-center items-center">
-      {/* Overlay Gelap agar teks terbaca */}
       <div className="bg-black/30 absolute inset-0"></div>
 
-      <div className="w-1/3 bg-white/10 backdrop-blur-md border border-white/20 rounded-3xl p-6 m-auto flex flex-col content-center items-center">
+      <div className="relative w-full max-w-md bg-white/10 backdrop-blur-md border border-white/20 rounded-3xl shadow-2xl p-8 m-auto flex flex-col content-center items-center gap-8">
         <div className="w-full flex gap-4">
           <input
             type="text"
@@ -23,25 +14,30 @@ function App() {
             className="w-full bg-white/10 px-4 py-2 rounded-3xl border border-white/20 text-white"
           />
           <button className="bg-white/10 backdrop-blur-md border border-white/20 p-2 rounded-full text-white">
-            <Search />
+            🔍
           </button>
         </div>
-        <div>Jakarta, ID</div>
-        <div>
-          <p>28 C</p>
-        </div>
-        <div>
-          <p>gambar, cerah berawan</p>
-        </div>
-        <div>Border</div>
-        <div>
+        <div className="flex flex-col content-center items-center gap-3">
           <div>
-            <p>Kelembaban</p>
-            <p>75%</p>
+            <p className="text-white font-medium text-3xl">Jakarta, ID</p>
           </div>
           <div>
+            <p className="text-white font-bold text-8xl">28°C</p>
+          </div>
+          <div>
+            <p className="text-white font-light text-xl">
+              ⛅ <span className="capitalize">cerah berawan</span>
+            </p>
+          </div>
+        </div>
+        <div className="w-full pt-6 border-t border-white/20 grid grid-cols-2 place-items-center text-sm text-white/80 font-light">
+          <div className="grid grid-rows-2 place-items-center">
+            <p>Kelembaban</p>
+            <p className="font-semibold">75%</p>
+          </div>
+          <div className="grid grid-rows-2 place-items-center">
             <p>Kecepatan Angin</p>
-            <p>12 km/h</p>
+            <p className="font-semibold">12 km/h</p>
           </div>
         </div>
       </div>
